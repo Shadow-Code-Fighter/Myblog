60de9bdc999fb6aa7c05fe1452b6e9ab8fd7e58d
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-23 14:59:11 +0530

    127: Create a blog and added entity class Date 4-1-24 added PostMapping, added GetMapping and Exception Handling

--- src/main/java/com/myblog/controller/PostController.java
+++ src/main/java/com/myblog/controller/PostController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -22,9 +24,22 @@ public class PostController {
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
-    @GetMapping
+//     http://localhost:8080/api/posts/all?id=1
+    @GetMapping("/all")
     public ResponseEntity<PostDto> getPostById(@RequestParam long id){
         PostDto dto = postService.getPostById(id);
         return new ResponseEntity<>(dto,HttpStatus.OK);
+    }
+
+//    localhost:8080/api/posts?pageNo=0&pageSize=3&sortBy=title&sortDir
+    @GetMapping
+    public List<PostDto> getAllPosts(
+            @RequestParam(name = "pageNo", required = false,defaultValue = "0") int pageNo,
+            @RequestParam(name = "pageSize",required = false,defaultValue = "3") int pageSize,
+            @RequestParam(name = "sortBy",required = false,defaultValue = "3") String sortBy,
+            @RequestParam(name = "sortDir",required = false,defaultValue = "3") String sortDir
+    ){
+        List<PostDto> postDtos = postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
+        return postDtos;
     }
 }
--- src/main/java/com/myblog/service/serviceimpl/PostServiceImpl.java
+++ src/main/java/com/myblog/service/serviceimpl/PostServiceImpl.java
@@ -5,7 +5,13 @@ import com.myblog.exception.ResourceNotFoundException;
 import com.myblog.payload.PostDto;
 import com.myblog.repository.PostRepository;
 import com.myblog.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -18,18 +24,11 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
 
+        Post post = mapToEntity(postDto);
         Post savedPost = postRepository.save(post);
 
-        PostDto dto = new PostDto();
-        dto.setTitle(savedPost.getTitle());
-        dto.setDescription(savedPost.getDescription());
-        dto.setContent(savedPost.getContent());
-
+        PostDto dto = mapToDto(savedPost);
         return dto;
     }
 
@@ -44,5 +43,34 @@ public class PostServiceImpl implements PostService {
         dto.setDescription(post.getDescription());
         dto.setContent(post.getContent());
         return dto;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        PageRequest pageable = PageRequest.of(pageNo, pageSize,sort);
+
+        Page<Post> pagePost = postRepository.findAll(pageable);
+        List<Post> posts = pagePost.getContent();
+
+        List<PostDto> dtos = posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+        return dtos;
+    }
+
+
+    PostDto mapToDto(Post post){
+        PostDto dto = new PostDto();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setDescription(post.getDescription());
+        dto.setContent(post.getContent());
+        return dto;
+    }
+    Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
     }
 }
